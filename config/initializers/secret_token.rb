d371e4de497a8d9b7a2e742cfe6a102ca6a761fe
kbaribeau <kevin.baribeau@gmail.com>
2010-10-19 20:19:55 -0400

    initial commit raw rails3 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DoRubyTuesday::Application.config.secret_token = '35f81027ab109c31023825712bfab06d22247d95b7b0e84be27a8def68a8069dd73e8212a030d407a5bdd5c54477e81eebec90312630b42e26e742780411f4d8'
