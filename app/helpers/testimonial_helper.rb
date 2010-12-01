7ae6d08eb0e527a522b044e34a6880b3046e993d
kbaribeau <kevin.baribeau@gmail.com>
2010-11-30 19:49:48 -0500

    random twitter stuff

--- app/helpers/testimonial_helper.rb
+++ app/helpers/testimonial_helper.rb
@@ -1,2 +1,11 @@
 module TestimonialHelper
+	def twitter_oauth_url
+		@consumer=OAuth::Consumer.new( "TFzFh0qEhFGjA5uHWAJBCQ","vxegxZn5AeZ3Tzl5NaMz4LsulAkFLH0BlIjo9vjkEEU", {
+			:site=>"https://agree2"
+		})
+
+		@request_token=@consumer.get_request_token
+		session[:request_token]=@request_token
+		@request_token.authorize_url
+	end
 end
